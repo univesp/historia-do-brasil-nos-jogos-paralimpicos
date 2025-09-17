fba364f3b84f1e461e6a627a86d64bcbfb997f36
Kauê Delfino <128550129+KDelfino@users.noreply.github.com>
2025-09-17 11:42:19 -0300

    Add files via upload

---
+++ javascripts/events.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+
+  //////////////////////////////////////////////////////////////////
+  // HEADER DINÂMICO
+  // Mostra header somente no início da página.
+  // Descomentar caso utilizada a classe .header-dinamico. Caso contrário, deletar.
+
+  $(window).scroll(function(){
+    var nav = $(".header-dinamico .container");
+    var scroll = $(window).scrollTop();
+    if(scroll == 0){
+      nav.fadeIn();
+    } else {
+      nav.fadeOut();
+    }
+  });
+
+
+
+  //////////////////////////////////////////////////////////////////
+
+  $(function(){
+
+  window.sr = ScrollReveal();
+
+  if ($(window).width() < 768) {
+
+    if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+      $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+    }
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+  } else {
+    
+    sr.reveal('.js--fadeInLeft', {
+      origin: 'left',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+  }
+  
+  sr.reveal('.js--fadeInLeft', {
+      origin: 'left',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+
+});
+
+});
